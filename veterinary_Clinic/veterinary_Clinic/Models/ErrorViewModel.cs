1f07b31bc6255a24fe879b91516efa7f682c42ce
username <user@email.com>
2022-12-08 10:09:27 +0200

    MVC

---
+++ veterinary_Clinic/veterinary_Clinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace veterinary_Clinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
